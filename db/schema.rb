f2c21894fc9d35df3af66b9f57086cce1112b2ae
Dan <dan.the.hudson@gmail.com>
2016-10-14 00:07:05 +0000

    corrected issues with projects controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009192850) do
+ActiveRecord::Schema.define(version: 20161013000316) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text   "description"
+    t.string "image"
   end
 
 end
