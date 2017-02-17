b8a35d1857358631e9462e16d1e8f125e1314746
Dan <dan.the.hudson@gmail.com>
2017-02-17 20:31:01 +0000

    added styling to project display

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,9 @@ skills = Skill.create([{name: 'Ruby', image_url: 'Ruby_logo.png' },
     {name: 'Sinatra', image_url: 'Sinatralogo.png'}, 
     {name: 'Wordpress', image_url: 'wp_logo.png'}])
     
-projects = Project.create([{name: 'e-Commerce Site', description: 'This is a fully functioning e-commerce website, built to showcase my nack for fullstack, and to solicit props for my rails chops.', image_url: 'Sophisticated_Airhead.png', url: 'https://shaman-swap.herokuapp.com'},
-    {name: 'Travel Package Site', description: 'A demo travel package site, complete with payment processing', image_url: '', url: ''},
-    {name: 'Cocktail Blog', description: '', image_url: '', url: ''},
-    {name: 'Recipe Sorting API', description: 'This project is a simple program built in Sinatra, which has both a browser interface and API', image_url: '', url: 'https://github.com/Zorganon/sort_challenge'},
-    {name: '', description: '', image_url: '', url: ''},
+projects = Project.create([{name: 'Sophisticated Airhead', description: 'This is a fully functioning e-commerce website, built to showcase my nack for fullstack, and to solicit props for my rails chops.', image_url: 'Sophisticated_Airhead.png', url: 'https://shaman-swap.herokuapp.com'},
+    {name: 'Travel Package Site', description: 'A demo travel package site, complete with payment processing', image_url: 'travel-blog.png', url: 'https://organon-travel.herokuapp.com'},
+    {name: 'Cocktail Blog', description: 'This is a project blog with comments, ratings, and archive', image_url: 'cocktail-blog.png', url: 'http://cocktail-blog.herokuapp.com'},
+    {name: 'Recipe Sorting API', description: 'This project is a simple program built in Sinatra, which has both a browser interface and API', image_url: 'Sinatralogo.png', url: 'https://github.com/Zorganon/sort_challenge'},
+    {name: 'Northern Lights Sailing Club', description: 'This is the live site for the Northern Lights Sailing Club.  Built using Wordpress, this site has many layers and features including protected member content.', image_url: 'nlsc.png', url: ''},
     ])
