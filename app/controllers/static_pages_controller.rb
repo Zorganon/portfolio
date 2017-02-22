13d20686d08a725d24587dc2857d8f823469e0e8
Dan <dan.the.hudson@gmail.com>
2017-02-22 19:47:43 +0000

    added actionMailer config and converted contact form to rails helpers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,11 @@ class StaticPagesController < ApplicationController
   def index
     @projects = Project.all
     @skills = Skill.all
+    @feelings = ["Happy", "Curious", 
+      "Anxious", "Stressed", "Hopeful", 
+      "Big League", "Nervous", "Angry", 
+      "Annoyed", "Irritated", "Joyful", 
+      "Tired", "Hungry", "Bashful", "I am the Pope"]
   end
 end
+
