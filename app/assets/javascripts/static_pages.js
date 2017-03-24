c5aa1912bfa4d709df9d5d171047cea9433c8ae2
Dan <dan.the.hudson@gmail.com>
2017-03-24 18:31:44 +0000

    added tooltip to skills

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,10 +1,18 @@
 /*global $*/
+
+//$(document).ready(function() {
+//  $('form').on("ajax:success", function(e, data, status, xhr) {
+//    $('form').css('display', 'none');
+//    $('.thanks-div').css('visibility', 'visible');
+//    console.log("hey, are you watchin' this?");
+//  }).on( "ajax:error", function(e, xhr, status, error){
+//    $('form').append("<p>ERROR</p>")
+//  });
+//});
+
+
 $(document).ready(function() {
-  $('#contact-form').on("ajax:success", function(e, data, status, xhr) {
-    $('#contact-form').css('display', 'none');
-    $('.thanks-div').css('visibility', 'visible');
-    console.log("hey, are you watchin' this?");
-  }).on( "ajax:error", function(e, xhr, status, error){
-    $('#contact-form').append("<p>ERROR</p>")
+  $('.skill-box').hover(function(){
+    $('.skill-box').tooltip();
   });
 });
